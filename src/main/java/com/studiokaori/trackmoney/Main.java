05d42ed09823ee8480bc1f47e83ca83d3b1256f2
kaoripersson <mkaori.1101@gmail.com>
2020-10-08 00:22:21 +0200

    git Branch test

--- src/main/java/com/studiokaori/trackmoney/Main.java
+++ src/main/java/com/studiokaori/trackmoney/Main.java
@@ -3,7 +3,11 @@ package com.studiokaori.trackmoney;
 import com.studiokaori.trackmoney.controller.MainController;
 
 public class Main {
+
     public static void main(String[] args) {
+
+        // added comment here for git pull test
         MainController mainController = new MainController();
+
     }
 }

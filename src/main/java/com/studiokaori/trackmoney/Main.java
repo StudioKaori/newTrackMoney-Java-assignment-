faa887ecd9383cd8767e8db0d1b770b3725f32dc
Kaori Persson <mkaori.1101@gmail.com>
2020-10-08 19:22:21 +0000

    Update Main.java

--- src/main/java/com/studiokaori/trackmoney/Main.java
+++ src/main/java/com/studiokaori/trackmoney/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
         // added comment here for git pull test
-        // changed this file on gitlab
         MainController mainController = new MainController();
 
     }

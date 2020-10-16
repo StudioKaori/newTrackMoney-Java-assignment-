2e981fe3d173296da80bee5192d7b580780496c3
kaoripersson <mkaori.1101@gmail.com>
2020-10-16 11:06:36 +0200

    Add Javadoc

---
+++ Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.studiokaori.trackmoney"},{"l":"com.studiokaori.trackmoney.controller"},{"l":"com.studiokaori.trackmoney.item"},{"l":"com.studiokaori.trackmoney.model"}]
